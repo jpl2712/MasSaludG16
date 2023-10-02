7a0ae67591d8878fe73a45ecafa12595eac6f691
LuciaEscobedo <101607876+LuciaEscobedo@users.noreply.github.com>
2023-10-02 17:38:12 -0300

    Entidades Afiliado

---
+++ src/Entidades/Afiliado.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Afiliado {
+    int idAfiliado;
+    String nombre;
+    String apellido;
+    int dni;
+    boolean activo;
+
+    public Afiliado() {
+    }
+
+    public Afiliado(int idAfiliado, String nombre, String apellido, int dni, boolean activo) {
+        this.idAfiliado = idAfiliado;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.activo = activo;
+    }
+
+    public int getIdAfiliado() {
+        return idAfiliado;
+    }
+
+    public void setIdAfiliado(int idAfiliado) {
+        this.idAfiliado = idAfiliado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Afiliado(String nombre, String apellido, int dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Afiliado{" + "idAfiliado=" + idAfiliado + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", activo=" + activo + '}';
+    }
+    
+}
